4250fa5f0a4c069a0067820fd1b06707c5311c88
Akademija <akademija@Akademijas-MBP.fritz.box>
2024-03-05 21:00:46 +0100

    Created project

---
+++ SofascoreApp/SofascoreApp/SofascoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SofascoreAppApp.swift
+//  SofascoreApp
+//
+//  Created by Akademija on 05.03.2024..
+//
+
+import SwiftUI
+
+@main
+struct SofascoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
